526944dfd7a8498137bc442a7ed15030e5f15dad
junhuanchen <junhuanchen@qq.com>
2021-11-03 10:35:52 +0800

    [update] 0.3.5.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name="rpyc_ikernel",
-    version="0.3.4",
+    version="0.3.5",
     description="rpyc for jupyter kernel",
     author="Juwan",
     author_email="juwan@sipeed.com",
